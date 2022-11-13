29f11ede88f6cd0749f0a6e5f30192bc136aea1e
Daniel <danielcoco96@gmail.com>
2022-11-13 19:18:22 +0100

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+#include "Audio.h"
+#include "SD.h"
+#include "FS.h"
+
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
