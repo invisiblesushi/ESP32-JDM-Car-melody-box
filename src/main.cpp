10ea4de844ec9707c7a98ffb0687e867b129276e
Daniel <danielcoco96@gmail.com>
2022-11-13 20:35:57 +0100

    Mp3 playoff, read every file in directory

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,73 @@
 #include <Arduino.h>
-#include "Audio.h"
-#include "SD.h"
-#include "FS.h"
+#include <Audio.h>
+#include <SD.h>
+#include <FS.h>
+
+// SD card connections
+#define SD_CS 5
+#define SD_MOSI 23
+#define SD_MISO 19
+#define SD_CLK 18
+
+// Max98357 I2S connections
+#define I2S_DIN 22
+#define I2S_BCLK 26
+#define I2S_LRC 25
+
+// Audio object
+Audio audio;
+
+File dir;
+
 
 
 void setup() {
-  // put your setup code here, to run once:
+  // Initialize serial port
+  Serial.begin(115200);
+
+  // Initialize SD card
+  pinMode(SD_CS, OUTPUT);      
+  digitalWrite(SD_CS, HIGH); 
+  SPI.begin(SD_CLK, SD_MISO, SD_MOSI);
+
+  if(!SD.begin(SD_CS))
+  {
+    Serial.println("Error accessing microSD card");
+    while(true); 
+  }
+
+  dir = SD.open("/mp3");
+
+  // Setup I2S 
+  audio.setPinout(I2S_BCLK, I2S_LRC, I2S_DIN);
+    
+  // Set Volume
+  audio.setVolume(30);
+    
+  // Open music file
+  audio.connecttoFS(SD,"mp3/jdmsafedrivesequence2.mp3");
+
+
+  
+  int numberOfFiles = 0;
+  String files[100] = {};
+
+  // prints all files in directory
+  while (true)
+  {
+    File entry = dir.openNextFile();
+
+    if (! entry)
+    {
+      break;
+    }
+
+    Serial.println(entry.name());
+  };
+
+  Serial.println(numberOfFiles);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    audio.loop();    
 }
